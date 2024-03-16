b93a9050489436c2fc419a50bcc6baa4afcfec14
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-15 22:06:45 -0600

    fix: error in background to App | dev: angelcruz07

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className='bg-bg-gray h-screen'>
+		<main className=''>
 			<h1 className='text-tom-thumb-500'>Y PURO MODO GUERRA MIS COMPAS</h1>
 		</main>
 	)
