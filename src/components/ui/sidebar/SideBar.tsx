d70264e0f83bbd41db8824e35d924ff425737827
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-16 10:21:08 -0600

    ci: Add Side menu functionality

--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -1,55 +1,61 @@
-import { IconHome } from '@tabler/icons-react'
+'use client'
+import { useState } from 'react'
 import Image from 'next/image'
-
-const rutes = [
-  {
-    name: 'Inicio',
-    icon: <IconHome stroke={2} className='text-color-icon' />,
-  },
-]
-
-// Hay que hacer una clase active se hace haciendo un estado setIsActive isActive y la condicion sera
-// isActive? '(estilos al momento de clickear)'
-// Usando arreglo rutes hacer un map que cada que hagan clic en un item del map, se ponga isActive para que active los estilos
+import { IconMenu2 } from '@tabler/icons-react'
+import Link from 'next/link'
 
 export const SideBar = () => {
-  return (
-    <div dir='rtl' className=''>
-      <div className='fixed w-1/2 flex flex-col items-center justify-start bg-tom-thumb-200 min-h-screen'>
-        <div className='flex items-center'>
-          <Image
-            src='https://res.cloudinary.com/dlklqucye/image/upload/v1710562810/mindflow-logo.webp'
-            alt='Logo Mindflow'
-            height={100}
-            width={100}
-          />
-          <span className='font-bold uppercase text-text-logo'>mindflow</span>
-        </div>
-        <div className='flex flex-col gap-4'>
-          <div>{/* Profile Image */}</div>
-          <div>
-            <span className='text-color-tex-pr text-sm'>¡Qué bueno verte!</span>
-            <p className='font-semibold text-color-text-name-profile text-lg'>
-              Mike Rooss
-            </p>
-          </div>
-          <div>
-            <span className='bg-gradient-to-r from-color-box to-color-box-secundary p-2 font-semibold text-white rounded-md'>
-              helpful
-            </span>
-          </div>
-          <hr className='w-full h-1 mx-auto my-4 bg-color-separator border-0 rounded md:my-10' />
-        </div>
-        <div>
-          <ul>
-            <li className=' font-semibold flex items-center gap-2 text-lg'>
-              <span>Inicio</span>
-              <IconHome stroke={2} className='text-color-icon' />
-            </li>
-            <li></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
+
+	const closeMenu = () => {
+		setIsOpen(false)
+	}
+
+	return (
+		<>
+			<nav className='fixed top-0 z-50 w-full md:w-72 bg-white shadow-lg md:shadow-none'>
+				<div className='px-3 py-3 lg:px-5 lg:pl-3'>
+					<div className='flex items-center justify-between'>
+						<div className='flex items-center justify-start rtl:justify-end'>
+							<button
+								onClick={toggleMenu}
+								className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
+								<IconMenu2 size={24} />
+							</button>
+							<Link href='/' className='flex ms-2 md:me-24'>
+								<Image
+									src='https://res.cloudinary.com/dlklqucye/image/upload/v1710562810/mindflow-logo.webp'
+									className=''
+									alt='MindFlow'
+									width={70}
+									height={70}
+								/>
+								<span className='self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-tom-thumb-500'>
+									Mind Flow
+								</span>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</nav>
+
+			<aside
+				className={`fixed top-0 left-0 z-40 w-72 h-screen pt-20 transition-transform ${
+					isOpen ? 'translate-x-0' : '-translate-x-full'
+				} bg-white sm:translate-x-0 `}>
+				<div className='mt-10'>
+					<h1>Hola</h1>
+				</div>
+			</aside>
+			{isOpen && (
+				<div
+					onClick={closeMenu}
+					className='fixed inset-0 bg-black opacity-50 z-30'></div>
+			)}
+		</>
+	)
 }
