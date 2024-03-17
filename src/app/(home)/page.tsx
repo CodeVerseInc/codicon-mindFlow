da22f506c1edb700f549a9229c672d45045c6510
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-17 08:56:24 -0600

    feat: advance in home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,90 @@
+import { CardBookBetter, CardRecurses } from '@/components'
+import { IconCornerDownRight } from '@tabler/icons-react'
+import Link from 'next/link'
+
+const personalDevelopmentResources = [
+	{
+		id: 1,
+		title: 'Desarrollo Personal: Mejora tu Autoestima',
+		link: 'https://www.youtube.com/@ELTEMACH'
+	},
+	{
+		id: 2,
+		title: 'Desarrollo Personal: Gestión del Tiempo',
+		link: 'https://www.youtube.com/@ELTEMACH'
+	},
+	{
+		id: 3,
+		title: 'Desarrollo Personal: Habilidades de Comunicación',
+		link: 'https://www.youtube.com/@ELTEMACH'
+	}
+]
+
+const books = [
+	{
+		title: 'El club de la 5 am',
+		image:
+			'https://res.cloudinary.com/dlklqucye/image/upload/v1710685121/unnamed_bfzvkc.webp',
+		description:
+			'El Club de las 5 de la mañana: Controla tus mañanas, impulsa tu vida / The 5 AM Club: Own Your Morning. Elevate Your Life. (Spanish Edition). ¡Oferta!'
+	},
+	{
+		title: 'Deja de pensar demasiado',
+		image:
+			'https://res.cloudinary.com/dlklqucye/image/upload/v1710685679/image_2_fcunvl.webp',
+		description:
+			'23 técnicas para aliviar el estrés, detener las espirales negativas, despejar la mente y concentrarse en el presente'
+	}
+]
+
 export default function Home() {
-	return <h1 className='text-tom-thumb-500 bg-black '>Home</h1>
+	return (
+		<section className='flex justify-between flex-col md:flex-row'>
+			<article className='w-full h-screen'>
+				<p>aqui el timer</p>
+				<article>aqui los post</article>
+			</article>
+			<article className='bg-white dark:bg-bg-dark w-full md:w-[750px] h-screen p-5'>
+				<div className='flex justify-between items-center'>
+					<h2 className=' text-xl font-bold'>Aqui los libros mas destacados</h2>
+					<Link
+						href='library'
+						className='text-xs font-extralight flex items-center'>
+						Ver todos
+						<IconCornerDownRight stroke={2} />
+					</Link>
+				</div>
+				<div>
+					{books.map((book, index) => (
+						<CardBookBetter
+							key={index}
+							title={book.title}
+							image={book.image}
+							description={book.description}
+						/>
+					))}
+				</div>
+
+				<div className='flex justify-between items-center'>
+					<h2 className='text-xl font-bold'>Recursos destacados</h2>
+					<Link
+						href='recurses'
+						className='text-xs font-extralight flex items-center'>
+						Ver todos
+						<IconCornerDownRight stroke={2} />
+					</Link>
+				</div>
+
+				<div className='flex gap-5 flex-col'>
+					{personalDevelopmentResources.map((resource) => (
+						<CardRecurses
+							key={resource.id}
+							title={resource.title}
+							link={resource.link}
+						/>
+					))}
+				</div>
+			</article>
+		</section>
+	)
 }
---
+++ src/components/ui/cards/CardRecurses.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+interface CardRecursesProps {
+	title: string
+	link: string
+}
+
+export const CardRecurses = ({ title, link }: CardRecursesProps) => {
+	return (
+		<div className='bg-white h-22 rounded-lg p-5 flex '>
+			<Link
+				href={link}
+				target='_blank'
+				className='flex justify-center items-center gap-x-5'>
+				<div>
+					<h4 className=' font-bold text-black'>{title}</h4>
+				</div>
+
+				<picture className='bg-bg-gray h-10 w-10 rounded-full flex items-center justify-center '>
+					<Image
+						src='https://res.cloudinary.com/dlklqucye/image/upload/v1710686627/solar_play-broken_iqw8hx.webp'
+						alt=''
+						width={20}
+						height={20}
+					/>
+				</picture>
+			</Link>
+		</div>
+	)
+}
