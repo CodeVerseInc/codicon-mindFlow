904a53e02b3488796f3d7c59b6ff129a7823c381
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-03-16 22:07:10 -0600

    feat: better in responsive desing

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 
 function SignIn() {
 	return (
-		<section className='flex justify-center items-center [grid-area:main] mt-8'>
-			<div className='bg-white p-10 flex flex-col-reverse  md:flex-row md:justify-between md:w-[1200px] rounded-lg '>
+		<section className='flex justify-center items-center [grid-area:main] md:mt-4 2xl:mt-8'>
+			<div className='bg-white p-5 2xl:p-10 flex flex-col-reverse md:flex-row  md:gap-5 md: justify-center 2xl:justify-between md:items-center sm:w-[900px] 2xl:w-[1200px]  rounded-lg '>
 				<article className='rounded-lg w-full md:w-[480px] '>
 					<picture>
 						<img
@@ -28,7 +28,7 @@ function SignIn() {
 					<FormSignin />
 					{/* Login with Google */}
 					<LoginSignInGoogle>Iniciar sesion con Google</LoginSignInGoogle>
-					<div className='text-center mt-10'>
+					<div className='text-center md:mt-5 2xl:mt-10'>
 						<Link href='login'>
 							Ya tienes cuenta?{' '}
 							<span className='text-tom-thumb-950 font-semibold '>
@@ -43,9 +43,9 @@ function SignIn() {
 						<img
 							src='https://res.cloudinary.com/dlklqucye/image/upload/v1710562891/img-login.webp'
 							alt='Imagen inicio sesion'
-							className='rounded-lg '
-							width={610}
-							height={540}
+							className='rounded-lg 2xl:w-[610px] 2xl:h-[520px]'
+							width={300}
+							height={520}
 						/>
 					</picture>
 				</article>
