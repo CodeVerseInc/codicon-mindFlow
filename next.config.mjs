02d492a240a0df241d388f33512cca338c3a866b
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-16 20:47:45 -0600

    fix: error in next config | dev: angelcruz07

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
+		domains: [
 			'res.cloudinary.com',
 			'lh3.googleusercontent.com',
 			'via.placeholder.com',
