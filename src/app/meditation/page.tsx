b276e2a2d4bd2d91971608d1887b9525c6b5808b
Ariff-dev <ariff.dev24@gmail.com>
2024-03-17 11:12:11 -0600

    fix: h-screen

--- src/app/meditation/page.tsx
+++ src/app/meditation/page.tsx
@@ -2,12 +2,12 @@ import { Timer } from '@/components/timer'
 import { Calendar } from '@/components/timer/components/Calendar'
 
 export default function Meditation() {
-	return (
-		<main className='flex flex-col justify-center items-center'>
-			<Timer />
-			<div>
-				<Calendar />
-			</div>
-		</main>
-	)
+  return (
+    <main className='flex flex-col justify-center items-center'>
+      <Timer />
+      <div>
+        <Calendar />
+      </div>
+    </main>
+  )
 }
