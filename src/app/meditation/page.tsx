0da07113076c9982f82092a53748acce5fff6daf
Ariff-dev <ariff.dev24@gmail.com>
2024-03-17 10:54:40 -0600

    feat: Calendar Component

--- src/app/meditation/page.tsx
+++ src/app/meditation/page.tsx
@@ -1,13 +1,13 @@
 import { Timer } from '@/components/timer'
-import { Mood } from '@/components/timer/components/Mood'
+import { Calendar } from '@/components/timer/components/Calendar'
 
 export default function Meditation() {
   return (
     <main className='flex flex-col justify-center items-center'>
       <Timer />
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <Calendar />
+      </div>
     </main>
   )
 }
